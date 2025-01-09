31fbecc338ca24dc51dca84e68b37ec478aae3fc
BarreraPeres <gabrielbarreraperes18@gmail.com>
2025-01-09 13:53:44 -0300

    feat: add suport for downloading more types of links
    - sharing links
    - links with timestamps
    - indexed links
    - mix links

--- frontend/src/functions/get-id-by-link.js
+++ frontend/src/functions/get-id-by-link.js
@@ -1,5 +1,52 @@
 export async function getIdByLink(url) {
+
+    if (url.includes("&start_radio=")) {
+        console.info("link de mix de playlist")
+        const [link, mix_link] = url.split("&start_radio=")
+
+        const data = await separeteIdPlaylistAndIdVideo(link)
+
+        return { id_playlist: data.id_playlist, id_video: data.id_video }
+
+    }
+
+    if (url.includes("t=")) {
+        console.info("link contém tempo")
+        const [link, time] = url.split("&t=")
+
+        const [youtube, id_video] = link.split("?v=")
+
+        return { id_video }
+    }
+
+    if (url.includes("?si=")) {
+        console.info("link de compartilhamento: ", url)
+        const [link, parms] = url.split("?si=")
+
+        const [youtube, id_video] = link.split("youtu.be/")
+
+        return { id_video }
+
+    }
+
+
+    if (url.includes("&index")) {
+        console.info("link com index da playlist")
+        const [link, index] = url.split("&index=")
+
+        const data = await separeteIdPlaylistAndIdVideo(link)
+
+        return { id_playlist: data.id_playlist, id_video: data.id_video }
+    }
+
+    const data = await separeteIdPlaylistAndIdVideo(url)
+
+    return { id_playlist: data.id_playlist, id_video: data.id_video }
+}
+
+async function separeteIdPlaylistAndIdVideo(url) {
     const [link, parms] = url.split("?v=")
+
     if (!parms) {
         const [url_youtube, id_playlist] = link.split("?list=")
         return { id_playlist }
@@ -7,6 +54,5 @@ export async function getIdByLink(url) {
         const [id_video, id_playlist] = parms.split("&list=")
         return { id_playlist, id_video }
     }
-
 
 }
