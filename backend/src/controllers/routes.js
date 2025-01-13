71e0b841aa4e708b23a50c762645a03ec277abb2
BarreraPeres <gabrielbarreraperes18@gmail.com>
2025-01-12 23:42:26 -0300

    feat: add route for download mp3 and mp4
    update package*.json install ffmpeg

--- backend/src/controllers/routes.js
+++ backend/src/controllers/routes.js
@@ -1,6 +1,8 @@
-import { downloadController } from "./download.js";
+import { downloadControllerMp3 } from "./download-mp3.js";
+import { downloadControllerMp4 } from "./download-mp4.js";
 
 export async function routes(app) {
 
-    app.get("/download/:id_video", downloadController)
+    app.get("/download/:id_video/mp3", downloadControllerMp3)
+    app.get("/download/:id_video/mp4", downloadControllerMp4)
 }
