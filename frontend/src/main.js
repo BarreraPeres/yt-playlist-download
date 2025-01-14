60efff29774b5878836cbd0d87de8c4d7aa33618
BarreraPeres <gabrielbarreraperes18@gmail.com>
2025-01-13 23:05:23 -0300

    feat: add select to choose video type (mp4 or mp3)

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,58 +1,87 @@
 import './index.css'
-import { endLoader, startLoader } from './utils/loader.js'
-import { ApiYoutube } from './lib/api-youtube.js'
-import { queue } from './functions/queue.js'
-import { getIdByLink } from './functions/get-id-by-link.js'
+import { formHandleDownloadLink } from './functions/form-handle-download-link.js'
 
 document.querySelector('#app').innerHTML = `
 
   <form id="FormYT" class="flex mb-6">
     <input 
-    class="text-sm text-zinc-300 border border-emerald-800 hover:border-emerald-950 rounded-lg hover:bg-emerald-950 bg-emerald-900 focus:ring-emerald-950 focus:border-emerald-950 p-3 ps-10 w-[540px]"
+    class="
+      text-sm
+    text-zinc-300 
+      border
+    border-emerald-800
+    hover:border-emerald-950
+      rounded-lg 
+    hover:bg-emerald-950
+    bg-emerald-900 
+    focus:ring-emerald-950
+    focus:border-emerald-950
+      p-3 
+      ps-10 
+      w-[540px]"
     placeholder="Baixe em Mp3, link de um vídeo ou de uma playlist do YouTube..."
     id="link"
     > 
-      <button class="bg-emerald-900 py-4 px-8 rounded-md ml-1 hover:bg-emerald-950">
-      <svg class="text-zinc-300" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-arrow-down-to-line"><path d="M12 17V3"/><path d="m6 11 6 6 6-6"/><path d="M19 21H5"/></svg>
-      </button>
-      </input> 
+
+    <select
+      name="dropdown"     
+      id="dropdown" 
+      class="
+      text-sm
+      text-zinc-300
+      bg-emerald-900
+      hover:bg--950
+      ml-1
+      py-1
+      px-4
+      rounded-lg" 
+      >
+        <option
+          value="mp3"
+          >.Mp3
+        </option>
+        <option
+          value="mp4"
+          >.Mp4
+        </option>
+    </select>
+
+    </input>
+    
   </form>
-
-  <div id="ytplayer" class="flex w-[640px] h-[360px] bg-zinc-900 rounded-lg aspect-video"></div>
+ 
+    <div 
+      id="ytplayer"
+      class="
+      flex
+      w-[640px]
+      h-[360px]
+    bg-zinc-900
+      rounded-lg
+      aspect-video"
+    >
   
+      <button
+        id="download"
+        class="
+        absolute
+        ml-[542px]
+        mt-[300px]
+        bg-emerald-900
+        py-3
+        px-8
+        rounded-md
+        hover:bg-emerald-950
+        "
+        >
+          <svg class="text-zinc-300" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-arrow-down-to-line"><path d="M12 17V3"/><path d="m6 11 6 6 6-6"/><path d="M19 21H5"/></svg>
+      </button>
+
+    </div>
+ 
 `
+document.getElementById("download").addEventListener("click", async (e) => {
 
-document.getElementById("FormYT").addEventListener("submit", async (e) => {
-    e.preventDefault()
-    const url = document.getElementById("link").value
-    const { id_playlist, id_video } = await getIdByLink(url)
-
-    if (!id_playlist && !id_video) {
-        alert("Link inválido!!")
-        return
-    }
-
-    try {
-        await startLoader()
-
-        if (!id_playlist) {
-            const yt = new ApiYoutube()
-            console.log("inicando o download do seu video")
-            await yt.videoLoad(id_video)
-            await queue()
-        } else {
-            const yt = new ApiYoutube()
-            console.log("inicando o download da sua playlist")
-            await yt.playlistLoad(id_playlist)
-            await queue()
-        }
-    } catch (e) {
-        console.log(e)
-    } finally {
-        endLoader()
-    }
-
-
-
+  await formHandleDownloadLink()
 
 })
