81e9b993c167d4178cbcc3dc7713473d76a30fcd
BarreraPeres <gabrielbarreraperes18@gmail.com>
2025-01-16 02:44:07 -0300

    fix: close issue #5
    remove run() e add trycath

--- backend/src/use-cases/ffmpeg-joining-mp3-mp4.js
+++ backend/src/use-cases/ffmpeg-joining-mp3-mp4.js
@@ -5,13 +5,17 @@ export async function ffmpegJoiningMp3Mp4(filepathMp4, filepathMp3, filepath) {
     ffmpeg.setFfmpegPath(ffmpegPath);
 
     return new Promise((resolve, reject) => {
-        ffmpeg()
-            .addInput(filepathMp4)
-            .addInput(filepathMp3)
-            .saveToFile(filepath)
-            .run()
-            .on("end", () => {
-                resolve()
-            })
+        try {
+            ffmpeg()
+                .addInput(filepathMp4)
+                .addInput(filepathMp3)
+                .saveToFile(filepath)
+                .on("end", () => {
+                    resolve()
+                })
+        } catch (error) {
+            reject()
+        }
+
     })
 }
