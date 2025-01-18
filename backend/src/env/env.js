4cb49cebe4460d2cab5aa3d049edfc88b01324c9
BarreraPeres <gabrielbarreraperes18@gmail.com>
2025-01-18 18:11:00 -0300

    feat: modiefied env-exemple and add cookies in env

--- backend/src/env/env.js
+++ backend/src/env/env.js
@@ -4,7 +4,10 @@ const envSchema = z.object({
     PORT: z.coerce.number().default(3333),
     HOST: z.string().default("0.0.0.0"),
     AUDIO_TEST_FILEPATH: z.string().default(""),
-    CORS_ORIGIN: z.string().default("*")
+    CORS_ORIGIN: z.string().default("*"),
+    YOUTUBE_COOKIE_HSID: z.string(),
+    YOUTUBE_COOKIE_SSID: z.string(),
+    YOUTUBE_COOKIE_SID: z.string()
 })
 
 const _env = envSchema.safeParse(process.env)
@@ -13,6 +16,5 @@ if (_env.success === false) {
     console.log("ERROR IN ENVIRONMENT VARIABLES", _env.error.format())
     throw new Error("ERROR IN ENVIRONMENT VARIABLES")
 }
-
 
 export const env = _env.data
