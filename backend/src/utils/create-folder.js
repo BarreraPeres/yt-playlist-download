554e33a9fcf4fd8260e6c7d788c251e2803305f9
BarreraPeres <gabrielbarreraperes18@gmail.com>
2025-01-19 02:03:41 -0300

    CI: add try catch in create folder for error in deployment

--- backend/src/utils/create-folder.js
+++ backend/src/utils/create-folder.js
@@ -3,12 +3,19 @@ import fs from "node:fs";
 import path from "node:path";
 
 export async function createFolder() {
-    let folder;
-    if (!fs.existsSync("./src/downloads/")) {
-        folder = await mkdir("./src/downloads/", { recursive: true })
-    } else {
-        folder = path.join(process.cwd(), "src", "downloads")
-    }
+    try {
+        let folder;
+        if (!fs.existsSync("./src/downloads/")) {
+            folder = await mkdir("./src/downloads/", { recursive: true })
+        } else {
+            folder = path.join(process.cwd(), "src", "downloads")
+        }
 
-    return folder
+        await fs.promises.access(folder, fs.constants.W_OK);
+
+        return folder
+    } catch (err) {
+        console.log(err)
+        throw err
+    }
 }
