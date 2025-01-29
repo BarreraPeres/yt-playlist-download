1a57a9c34117dfab7581141e52b71696a3be8c93
BarreraPeres <gabrielbarreraperes18@gmail.com>
2025-01-29 02:13:49 -0300

    refactor: remove unesed use cases
    refactor exclude-download-${fileFormat}, from two use cases to one
    remove ffmpeg

---
+++ backend/src/use-cases/exclude-download.js
@@ -0,0 +1,20 @@
+import { readdir } from "node:fs/promises"
+import { unlink } from "node:fs/promises"
+import path from "node:path"
+
+export async function excludeDownload(fileFormat) {
+
+    try {
+        const files = await readdir("./src/downloads/")
+
+        const mp3Files = files.filter((f) => f.endsWith(fileFormat))
+
+        for (const f of mp3Files) {
+            const filepath = path.resolve("./src/downloads/" + f)
+            await unlink(filepath)
+            console.log("Arquivo Excluido: ", f)
+        }
+    } catch (e) {
+        console.log(e)
+    }
+}
