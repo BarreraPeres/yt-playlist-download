d09f608c82e5fc15a3fd346ebb7e31d2c6f5dc3d
BarreraPeres <gabrielbarreraperes18@gmail.com>
2025-01-29 02:07:29 -0300

    feat: add dirname for read env
    update package*.json

--- backend/src/env/env.js
+++ backend/src/env/env.js
@@ -1,10 +1,18 @@
 import { z } from "zod";
+import dotenv from "dotenv"
+import path from "node:path"
+import dirname from "../../dirname.js";
+
+dotenv.config({ path: path.resolve(dirname, ".env"), })
 
 const envSchema = z.object({
     PORT: z.coerce.number().default(3333),
     HOST: z.string().default("0.0.0.0"),
     AUDIO_TEST_FILEPATH: z.string().default(""),
-    CORS_ORIGIN: z.string().default("*")
+    CORS_ORIGIN: z.string().default("*"),
+    YOUTUBE_COOKIE_HSID: z.string(),
+    YOUTUBE_COOKIE_SSID: z.string(),
+    YOUTUBE_COOKIE_SID: z.string(),
 })
 
 const _env = envSchema.safeParse(process.env)
